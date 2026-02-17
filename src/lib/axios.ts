fb2ba8408fe90c6dd35be1ad6e45f76bd18cec98
lemxvos <lemxvos@gmail.com>
2026-02-17 20:16:49 +0000

    v4

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://continuum-backend.onrender.com";
+const API_BASE = import.meta.env.VITE_API_BASE_URL; // Change this to your actual API base URL
 
 const api = axios.create({
   baseURL: API_BASE,
